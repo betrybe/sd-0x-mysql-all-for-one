53a71763b151ba24c77300b581c2550254c3acbc
thiagopnobre <thiago_pnobre@hotmail.com>
2020-07-10 17:09:39 -0300

    Typo

--- tests/tableManipulationChallenges.spec.js
+++ tests/tableManipulationChallenges.spec.js
@@ -141,7 +141,7 @@ describe('Desafios de manipulação de tabelas', () => {
         expect(await sequelize.query(countOrderDetailsQuery, { type: 'SELECT' }))
           .toEqual([{ details_count: 58 }]);
 
-        await sequelize.query(challengeQuery, { type: 'UPDATE' });
+        await sequelize.query(challengeQuery, { type: 'DELETE' });
 
         expect(await sequelize.query(countOrderDetailsQuery, { type: 'SELECT' }))
           .toEqual([{ details_count: 41 }]);
@@ -155,7 +155,7 @@ describe('Desafios de manipulação de tabelas', () => {
         expect(await sequelize.query(countOrderDetailsQuery, { type: 'SELECT' }))
           .toEqual([{ details_count: 58 }]);
 
-        await sequelize.query(challengeQuery, { type: 'UPDATE' });
+        await sequelize.query(challengeQuery, { type: 'DELETE' });
 
         expect(await sequelize.query(countOrderDetailsQuery, { type: 'SELECT' }))
           .toEqual([{ details_count: 20 }]);
@@ -169,7 +169,7 @@ describe('Desafios de manipulação de tabelas', () => {
         expect(await sequelize.query(countOrderDetailsQuery, { type: 'SELECT' }))
           .toEqual([{ details_count: 58 }]);
 
-        await sequelize.query(challengeQuery, { type: 'UPDATE' });
+        await sequelize.query(challengeQuery, { type: 'DELETE' });
 
         expect(await sequelize.query(countOrderDetailsQuery, { type: 'SELECT' }))
           .toEqual([{ details_count: 0 }]);
